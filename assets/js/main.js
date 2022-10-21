3d8de8772d985bce937ec58711d0846bd7863169
snerva <selenenerva@live.it>
2022-10-21 15:34:11 +0200

    initial commit and comments

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+//creare unh array con url, titolo e descrizione
+//creare markup statico di container e slide
+//rimuovi contenuto statico e utilizza l array di oggetti  per svilupparlo in modo dinamico 
+//al click a destra o sinistra l immagine attiva si vede e anche il relativo titolo/testo
+// aggiungere ciclo infinito. 
+
